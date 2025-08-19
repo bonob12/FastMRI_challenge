9e53f98f3c1ec8ac34e467c54cba47bccf3eb456
bonob12 <bono_b12@snu.ac.kr>
2025-08-19 10:40:15 +0900

    2.1

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -1,7 +1,2 @@
-pip install -r scripts/requirements.txt ; \
-pip install wandb  ; \
-pip install deepspeed  ; \
-pip install mpi4py  ; \
-pip uninstall torch ; \
-pip install torch==2.7.1+cu126 torchvision==0.22.1+cu126 --extra-index-url https://download.pytorch.org/whl/cu126
-
+pip install torch==2.7.1+cu126 torchvision==0.22.1+cu126 --extra-index-url https://download.pytorch.org/whl/cu126 ; \
+pip install -r scripts/requirements.txt
